1bde67dc7198a2cfb32a45cd84b4bf68ea8099c6
saripudin <arieptea@gmail.com>
2024-01-15 21:46:06 +0700

    commit

--- app/src/main/java/com/example/tugassplash/Tabfragmen1.java
+++ app/src/main/java/com/example/tugassplash/Tabfragmen1.java
@@ -1,19 +1,35 @@
 package com.example.tugassplash;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
 
 public class Tabfragmen1 extends Fragment {
-
+    CardView videoSatu;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmen_tab1, container, false);
+        View view = inflater.inflate(R.layout.fragmen_tab1, container, false);
+        videoSatu = (CardView) view.findViewById(R.id.satu);
+
+        videoSatu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentDetail = new Intent(getActivity(),MovieDetail.class);
+                startActivity(intentDetail);
+            }
+        });
+
+        return view;
     }
+
+
 }
+
+
